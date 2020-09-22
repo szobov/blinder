18c1a0228b347af63522685461aef8373bb9e400
Sergei Zobov <sergey@szobov.ru>
2020-09-22 16:29:08 +0300

    clang-formatted

--- sketch_sep20a.ino
+++ sketch_sep20a.ino
@@ -1,86 +1,81 @@
-#include "secrets.h"
 #include "data.h"
+#include "secrets.h"
 #include "web.h"
 
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 
-const char *ssid = STASSID;
-const char *password = STAPSK;
-
+const char* ssid = STASSID;
+const char* password = STAPSK;
 
 void setup(void) {
+    pinMode(Pin1, OUTPUT); // define pin for ULN2003 in1
+    pinMode(Pin2, OUTPUT); // define pin for ULN2003 in2
+    pinMode(Pin3, OUTPUT); // define pin for ULN2003 in3
+    pinMode(Pin4, OUTPUT); // define pin for ULN2003 in4
 
- pinMode(Pin1, OUTPUT);//define pin for ULN2003 in1
- pinMode(Pin2, OUTPUT);//define pin for ULN2003 in2
- pinMode(Pin3, OUTPUT);//define pin for ULN2003 in3
- pinMode(Pin4, OUTPUT);//define pin for ULN2003 in4
+    Serial.begin(115200); // initialize the serial monitor
 
-  Serial.begin(115200);//initialize the serial monitor
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    Serial.println("");
 
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(ssid, password);
-  Serial.println("");
+    // Wait for connection
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
 
-  // Wait for connection
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
+    Serial.println("");
+    Serial.print("Connected to ");
+    Serial.println(ssid);
+    Serial.print("IP address: http://");
+    Serial.println(WiFi.localIP());
 
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: http://");
-  Serial.println(WiFi.localIP());
-
-  server.on("/", handleRoot);
-  server.on("/motor", HTTP_GET, motorControl);
-  server.onNotFound(handleNotFound);
-  server.begin();
-  Serial.println("HTTP server started");
+    server.on("/", handleRoot);
+    server.on("/motor", HTTP_GET, motorControl);
+    server.onNotFound(handleNotFound);
+    server.begin();
+    Serial.println("HTTP server started");
 }
 
 void loop(void) {
-  server.handleClient();
-if(dirStatus ==1){
-   poleStep++;
-    driveStepper(poleStep);
- }else if(dirStatus ==2){
-   poleStep--;
-    driveStepper(poleStep);
- }else{
-  driveStepper(8);
- }
- if(poleStep>7){
-   poleStep=0;
- }
- if(poleStep<0){
-   poleStep=7;
- }
+    server.handleClient();
+    if (dirStatus == 1) {
+        poleStep++;
+        driveStepper(poleStep);
+    } else if (dirStatus == 2) {
+        poleStep--;
+        driveStepper(poleStep);
+    } else {
+        driveStepper(8);
+    }
+    if (poleStep > 7) {
+        poleStep = 0;
+    }
+    if (poleStep < 0) {
+        poleStep = 7;
+    }
 
-   delay(1);
+    delay(1);
 }
 
 void motorControl() {
-    if(server.arg(argId[0]) == "on")
-    {
-      dirStatus = 1;// CCW
-    }else if(server.arg(argId[0]) == "off"){
-      dirStatus = 3;  // motor OFF
-    }else if(server.arg(argId[1]) == "on"){
-      dirStatus = 2;  // CW
-    }else if(server.arg(argId[1]) == "off"){
-      dirStatus = 3;  // motor OFF
+    if (server.arg(argId[0]) == "on") {
+        dirStatus = 1; // CCW
+    } else if (server.arg(argId[0]) == "off") {
+        dirStatus = 3; // motor OFF
+    } else if (server.arg(argId[1]) == "on") {
+        dirStatus = 2; // CW
+    } else if (server.arg(argId[1]) == "off") {
+        dirStatus = 3; // motor OFF
     }
-  handleRoot();
-}//motorControl end
+    handleRoot();
+} // motorControl end
 
-
-void driveStepper(int c)
-{
-     digitalWrite(Pin1, pole1[c]);
-     digitalWrite(Pin2, pole2[c]);
-     digitalWrite(Pin3, pole3[c]);
-     digitalWrite(Pin4, pole4[c]);
-}//driveStepper end here
+void driveStepper(int c) {
+    digitalWrite(Pin1, pole1[c]);
+    digitalWrite(Pin2, pole2[c]);
+    digitalWrite(Pin3, pole3[c]);
+    digitalWrite(Pin4, pole4[c]);
+} // driveStepper end here
--- web.h
+++ web.h
@@ -8,106 +8,103 @@
 ESP8266WebServer server(80);
 
 void handleRoot() {
+    // Robojax.com ESP32 Relay Motor Control
+    String HTML = "<!DOCTYPE html>"
+                  "<html>"
+                  "<head>"
 
-   //Robojax.com ESP32 Relay Motor Control
- String HTML ="<!DOCTYPE html>"
-  "<html>"
-  "<head>"
-    
-"<title>Robojax 28BYJ-48 Motor Control</title>"
-    
-"<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
-  
-"<style>"
- 
-"html,body{"
-"width:100%;"
-"height:100%;"
-"margin:0}"
-"*{box-sizing:border-box}"
-".colorAll{"
-  "background-color:#90ee90}"
-".colorBtn{"
-  "background-color:#add8e6}"
-".angleButtdon,a{"
- " font-size:30px;"
-"border:1px solid #ccc;"
-"display:table-caption;"
-"padding:7px 10px;"
-"text-decoration:none;"
-"cursor:pointer;"
-"padding:5px 6px 7px 10px}a{"
-  "display:block}"
-".btn{"
-  "margin:5px;"
-"border:none;"
-"display:inline-block;"
-"vertical-align:middle;"
-"text-align:center;"
-"white-space:nowrap}";
+                  "<title>Robojax 28BYJ-48 Motor Control</title>"
 
-  HTML +="</style>"
-"</head>"
-"<body>"
-"<h1>Robojax 28BYJ-48 Stepper Motor Control </h1>"
-";";
+                  "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"
 
-    if(dirStatus ==2){
-    HTML +="<h2><span style=\"background-color: #FFFF00\">Motor Running in CW</span></h2>";    
-   }else if(dirStatus ==1){
-    HTML +="<h2><span style=\"background-color: #FFFF00\">Motor Running in CCW</span></h2>";      
-   }else{
-    HTML +="<h2><span style=\"background-color: #FFFF00\">Motor OFF</span></h2>";
-   }
-      if(dirStatus ==1){
-        HTML +="<div class=\"btn\"><a class=\"angleButton\" style=\"background-color:#f56464\"  href=\"/motor?";
+                  "<style>"
+
+                  "html,body{"
+                  "width:100%;"
+                  "height:100%;"
+                  "margin:0}"
+                  "*{box-sizing:border-box}"
+                  ".colorAll{"
+                  "background-color:#90ee90}"
+                  ".colorBtn{"
+                  "background-color:#add8e6}"
+                  ".angleButtdon,a{"
+                  " font-size:30px;"
+                  "border:1px solid #ccc;"
+                  "display:table-caption;"
+                  "padding:7px 10px;"
+                  "text-decoration:none;"
+                  "cursor:pointer;"
+                  "padding:5px 6px 7px 10px}a{"
+                  "display:block}"
+                  ".btn{"
+                  "margin:5px;"
+                  "border:none;"
+                  "display:inline-block;"
+                  "vertical-align:middle;"
+                  "text-align:center;"
+                  "white-space:nowrap}";
+
+    HTML += "</style>"
+            "</head>"
+            "<body>"
+            "<h1>Robojax 28BYJ-48 Stepper Motor Control </h1>"
+            ";";
+
+    if (dirStatus == 2) {
+        HTML += "<h2><span style=\"background-color: #FFFF00\">Motor Running in CW</span></h2>";
+    } else if (dirStatus == 1) {
+        HTML += "<h2><span style=\"background-color: #FFFF00\">Motor Running in CCW</span></h2>";
+    } else {
+        HTML += "<h2><span style=\"background-color: #FFFF00\">Motor OFF</span></h2>";
+    }
+    if (dirStatus == 1) {
+        HTML += "<div class=\"btn\"><a class=\"angleButton\" style=\"background-color:#f56464\"  "
+                "href=\"/motor?";
         HTML += argId[0];
         HTML += "=off\">";
-        HTML +=buttonTitle1[0]; //motor ON title
-      }else{
-        HTML +="	<div class=\"btn\"><a class=\"angleButton \" style=\"background-color:#90ee90\"  href=\"/motor?";  
-         HTML += argId[0];
-        HTML += "=on\">";       
-        HTML +=buttonTitle2[0];//motor OFF title   
-      }   
-     HTML +="</a>	</div>";  
-      if(dirStatus ==2){
-        HTML +="	<div class=\"btn\">		<a class=\"angleButton\" style=\"background-color:#f56464\"  href=\"/motor?";
+        HTML += buttonTitle1[0]; // motor ON title
+    } else {
+        HTML += "	<div class=\"btn\"><a class=\"angleButton \" "
+                "style=\"background-color:#90ee90\"  href=\"/motor?";
+        HTML += argId[0];
+        HTML += "=on\">";
+        HTML += buttonTitle2[0]; // motor OFF title
+    }
+    HTML += "</a>	</div>";
+    if (dirStatus == 2) {
+        HTML += "	<div class=\"btn\">		<a class=\"angleButton\" "
+                "style=\"background-color:#f56464\"  href=\"/motor?";
         HTML += argId[1];
         HTML += "=off\">";
-        HTML +=buttonTitle1[1]; //motor ON title
-      }else{
-        HTML +="	<div class=\"btn\"> <a class=\"angleButton \" style=\"background-color:#90ee90\"  href=\"/motor?";  
-         HTML += argId[1];
+        HTML += buttonTitle1[1]; // motor ON title
+    } else {
+        HTML += "	<div class=\"btn\"> <a class=\"angleButton \" "
+                "style=\"background-color:#90ee90\"  href=\"/motor?";
+        HTML += argId[1];
         HTML += "=on\">";
-        HTML +=buttonTitle2[1];//motor OFF title
-      }
-     HTML +="</a>	</div>";
+        HTML += buttonTitle2[1]; // motor OFF title
+    }
+    HTML += "</a>	</div>";
 
-  server.send(200, "text/html", HTML);  
-
+    server.send(200, "text/html", HTML);
 }
 
 void handleNotFound() {
+    String message = "File Not Found";
+    message += "URI: ";
+    message += server.uri();
+    message += "Method: ";
+    message += (server.method() == HTTP_GET) ? "GET" : "POST";
+    message += "Arguments: ";
+    message += server.args();
+    message += "";
 
-  String message = "File Not Found";
-  message += "URI: ";
-  message += server.uri();
-  message += "Method: ";
-  message += (server.method() == HTTP_GET) ? "GET" : "POST";
-  message += "Arguments: ";
-  message += server.args();
-  message += "";
+    for (uint8_t i = 0; i < server.args(); i++) {
+        message += " " + server.argName(i) + ": " + server.arg(i) + "";
+    }
 
-  for (uint8_t i = 0; i < server.args(); i++) {
-    message += " " + server.argName(i) + ": " + server.arg(i) + "";
-  }
-
-  server.send(404, "text/plain", message);
-
+    server.send(404, "text/plain", message);
 }
-
-
-
 
 #endif /* WEB_H */
